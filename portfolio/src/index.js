93e598a1751e6eca7a2a279aef728901f49db764
Simo Koskinen <simoko@uef.fi>
2020-10-26 16:05:43 +0200

    Portfolion tiedostoja

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,3 +1,4 @@
 function contentButton(element) {
+    document.getElementById("weeks").className = "navbar-collapse collapse";
     document.getElementById("contentFrame").src = "./weeks/" + element.id + ".html";
 }
